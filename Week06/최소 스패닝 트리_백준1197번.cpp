8b7c659956dbfdd2e98ccf158f65e7b3e0bfde31
SEOEUN <87869785+seoeunkong@users.noreply.github.com>
2023-12-27 11:29:09 +0900

    최소 스패닝 트리

--- Week06/최소 스패닝 트리_백준1197번.cpp
+++ Week06/최소 스패닝 트리_백준1197번.cpp
@@ -21,6 +21,7 @@ int Union(int a, int b) {
 	int n1 = Find(a);
 	int n2 = Find(b);
 
+	//연결하기 전에 사이클 생기는 것을 방지하기 위해 대표 노드가 같은지 검사 
 	if (n1 == n2) return -1;
 
 	n1 < n2 ? arr[n2] = n1 : arr[n1] = n2;
@@ -46,12 +47,13 @@ int main() {
 		vec.push_back(make_tuple(c, a, b));
 	}
 
+	//가중치 기준 오름차순으로 정렬 
 	sort(vec.begin(), vec.end());
 
 	int cnt = 0;
 	int answer = 0;
 	for (int i = 0; i < vec.size(); i++) {
-		if (cnt == v - 1) break;
+		if (cnt == v - 1) break; 
 
 		int result = Union(get<1>(vec[i]), get<2>(vec[i]));
 		if (result == -1) continue;
