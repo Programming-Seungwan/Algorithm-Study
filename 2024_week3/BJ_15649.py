c55e7617622de5c61dac0416807ea8092a263d35
SOojin_nii <80147944+SoozingKIM@users.noreply.github.com>
2024-01-30 22:37:37 +0900

    BJ15649 백트래킹풀이

---
+++ 2024_week3/BJ_15649.py
@@ -0,0 +1,23 @@
+# 15649 N과 M(1)
+# https://www.acmicpc.net/problem/15649
+
+import sys
+input=sys.stdin.readline
+
+N,M=map(int,input().split())
+result=[]
+visited=[False]*(N+1)
+
+def recur(num):
+    if num==M:
+        print(' '.join(map(str,result)))
+        return
+    for i in range(1, N+1):
+        if visited[i]==False:
+            visited[i]=True            
+            result.append(i)
+            recur(num+1)
+            visited[i]=False
+            result.pop()
+
+recur(0)
