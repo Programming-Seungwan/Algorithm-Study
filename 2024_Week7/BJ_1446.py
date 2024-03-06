360ba111f16ce6a36de17bb429febeaaa6b2654d
SOojin_nii <80147944+SoozingKIM@users.noreply.github.com>
2024-03-06 21:10:23 +0900

    Create BJ_1446.py

---
+++ 2024_Week7/BJ_1446.py
@@ -0,0 +1,37 @@
+# 1446 지름길
+
+import heapq
+import sys
+input = sys.stdin.readline
+INF=int(1e9)
+
+N,D = map(int, input().split())
+graph = [[] for _ in range(D+1)]
+
+distance = [INF]*(D+1)
+
+for i in range(D):
+    graph[i].append((1,i+1))
+
+for _ in range(N):
+    start,end,cost=map(int, input().split())
+    if end>D: 
+        continue
+    graph[start].append((cost, end))
+
+pq=[]
+heapq.heappush(pq,(0,0))
+
+while pq:
+    cur_cost, cur_v = heapq.heappop(pq)
+
+    if cur_cost>distance[cur_v]:
+        continue
+
+    for cost, next_v in graph[cur_v]:
+        next_cost=cur_cost+cost
+        if next_cost < distance[next_v]:
+            distance[next_v]=next_cost
+            heapq.heappush(pq,(next_cost,next_v))
+
+print(distance[D])
