13f879a58e16cbe2d6cf1eb56db6fed7bac73f0f
SOojin_nii <80147944+SoozingKIM@users.noreply.github.com>
2024-03-06 21:14:07 +0900

    BJ1916 다시 풀어보기 (더 공부 필요)

---
+++ 2024_Week7/BJ_1916.py
@@ -0,0 +1,31 @@
+# 1916 최소비용 구하기
+
+import heapq
+import sys
+input=sys.stdin.readline
+
+N = int(input())
+M = int(input())
+graph = [[] for _ in range(N+1)]
+
+for _ in range(M):
+    start,end,cost=map(int, input().split())
+    graph[start].append((cost, end))
+
+depature, arrival = map(int, input().split())      
+
+def dijkstra(depature,arrival):
+    costs={}
+    pq=[]
+    heapq.heappush(pq,(0,depature))
+
+    while pq:
+        cur_cost, cur_v = heapq.heappop(pq)
+        if cur_v not in costs:
+            costs[cur_v] = cur_cost
+            for cost, next_v in graph[cur_v]:
+                next_cost=cur_cost+cost
+                heapq.heappush(pq,(next_cost,next_v))
+    print(costs[arrival])
+
+dijkstra(depature,arrival)
