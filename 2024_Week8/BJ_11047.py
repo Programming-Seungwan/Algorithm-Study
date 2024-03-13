33b71c6fb5fbd19c431aed5bddc319f7e320963c
SOojin_nii <80147944+SoozingKIM@users.noreply.github.com>
2024-03-14 00:01:40 +0900

    BJ11047 그리디 풀이

--- 2024_Week8/BJ_11047.py
+++ 2024_Week8/BJ_11047.py
@@ -6,13 +6,13 @@ input=sys.stdin.readline
 
 n,k = map(int,input().split())
 coins = []
+count = 0
+
 for _ in range(n):
     coins.append(int(input()))
-
-cnt=0
 
 for i in range(n-1,-1,-1):
     cnt += k//coins[i]
     k = k%coins[i]
   
-print(cnt)
+print(count)
